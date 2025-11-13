1f97b53350b521c8ca67ae04206dd11f80ed33ef
Siannod <will.trewhiit@gmail.com>
2025-11-13 10:51:53 +0000

    setup combat - print

--- Text Game/src/combat.cpp
+++ Text Game/src/combat.cpp
@@ -1,2 +1,15 @@
 #pragma once
 #include "combat.h"
+
+
+void Combat::fill_field()
+{
+	for (int i = 0; i < 10; i++)
+	{
+
+		for (int j = 0; j < 10; j++)
+		{
+			
+		}
+	}
+}
--- Text Game/src/combat.h
+++ Text Game/src/combat.h
@@ -1,12 +1,23 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
+#include <map>
 
 class Combat
 {
 public:
-	std::string battle_field_top = "+-=-+";
-	std::string battle_field_mid = "|   |";
+
+	std::map <std::string, std::string> field_pieces =
+	{
+		{"top_first" , "+-=-+"},
+		{"mid_first", "|   |"},
+		{"top_rest", "-=-+"},
+		{"mid_rest", "   |"}
+	};
+
+	std::vector<std::string> battle_field;
 
 	//FUNCTIONS
+	void fill_field();
 };
